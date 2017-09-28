aa33e2a7d8577c854941403c310f0b84c9c05088
ianspan <ian_span@abv.bg>
2017-09-28 22:33:02 +0300

    homework

---
+++ Programing Fundamentals/Intro and Basic Syntax - Exercises/04. Beverage Labels/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energy = double.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+
+            double kal = volume * energy / 100;
+            double sug = volume / 100 * sugar;
+
+
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars", kal, sug);
+        }
+    }
+}
---
+++ Programing Fundamentals/Intro and Basic Syntax - Exercises/05. Character Stats/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterStats
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            int currhealth = int.Parse(Console.ReadLine());
+            int allhealth = int.Parse(Console.ReadLine());
+            int currenergy = int.Parse(Console.ReadLine());
+            int allenergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Health: |{0}{1}|", new string('|', currhealth), 
+                                                  new string('.', allhealth - currhealth));
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', currenergy), 
+                                                  new string('.', allenergy - currenergy));
+        }
+    }
+}
